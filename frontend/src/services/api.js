26e505f019f716ad3a2165114304aa0648b7ff65
David22003 <lakradavid396@gmail.com>
2025-12-22 23:04:59 +0530

    Hello

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+// const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://train-x-qz8o.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
