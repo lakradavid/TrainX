f05900dbefeffd8ea1ddc5f48cd78b8ed5834aae
David22003 <lakradavid396@gmail.com>
2025-12-25 22:47:44 +0530

    Fix API base URL to use VITE_API_URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+// const API_URL = 'http://localhost:5000/api';
 // const API_URL = 'https://train-x-qz8o.vercel.app/api';
-
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
+
 
 // Add token to every request if available
 api.interceptors.request.use((config) => {
